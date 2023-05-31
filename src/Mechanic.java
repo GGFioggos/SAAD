19b5cbef80b495cea32572890e93c74bdc8a88b5
VaratisG <varat2003@gmail.com>
2023-05-31 16:59:59 +0300

    adding attributes and basic methods to mechanic class

---
+++ src/Mechanic.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Mechanic {
+	private String name;
+	static ArrayList<Mechanic> MechanicCatalog = new ArrayList<Mechanic>();
+	static ArrayList<Task> tasks = new ArrayList<Task>();
+
+	public Mechanic(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static ArrayList<Mechanic> getMechanicCatalog() {
+		return MechanicCatalog;
+	}
+
+}
