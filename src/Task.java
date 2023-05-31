76aa89a9ff2cadfd02b4a0e4412c1b21b18965ac
moschidisg <mosxidismosxidis@gmail.com>
2023-05-31 17:46:04 +0300

    update Task class

--- src/Task.java
+++ src/Task.java
@@ -2,10 +2,18 @@ import java.util.ArrayList;
 
 public class Task {
 	static ArrayList<Task> tasksCatalog = new ArrayList<>();
-
+	
+	// Attributes
 	private String name;
 	private double cost;
-
+	
+	// Constructor
+	public Task(String name,double cost) {
+		this.name = name;
+		this.cost = cost;
+	}	
+	
+	// Methods
 	public double getCost() {
 		return cost;
 	}
