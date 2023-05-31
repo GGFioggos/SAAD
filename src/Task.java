200f984609ec739f8941f4b4e643e1a450a4ac1b
moschidisg <mosxidismosxidis@gmail.com>
2023-05-31 16:45:51 +0300

    Dummy class for testing

---
+++ src/Task.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Task {
+	private String name;
+	private double cost;
+	private ArrayList<Task> tasksCatalog = new ArrayList<>();
+	
+	
+	
+	public double getCost() {
+		return cost;
+	}
+}
