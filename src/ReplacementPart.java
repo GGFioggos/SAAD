78bcb7a360507762fcc5b476e20fc9389f87d8dd
moschidisg <mosxidismosxidis@gmail.com>
2023-05-31 17:47:08 +0300

    add ReplacementPart class

---
+++ src/ReplacementPart.java
@@ -0,0 +1,10 @@
+
+public class ReplacementPart {
+	private String name;
+	private int quantity;
+	
+	public ReplacementPart(String name, int quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	} 
+}
