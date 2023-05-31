2988f97836e6b1b8aa8d8d23c75d2179e007bfa4
moschidisg <mosxidismosxidis@gmail.com>
2023-05-31 17:47:22 +0300

    add Secretary class

---
+++ src/Secretary.java
@@ -0,0 +1,25 @@
+
+public class Secretary {
+	// Attributes
+	private int phoneNumber;
+	private String fullName;
+	
+	// Constructor
+	public Secretary(int phoneNumber, String fullName) {
+		this.phoneNumber = phoneNumber;
+		this.fullName = fullName;
+	}	
+	
+	// Methods
+	public void createCustomer() {
+		
+	}
+	
+	public void createVehicle() {
+		
+	}
+	
+	public void createAppointment() {
+		
+	}
+}
