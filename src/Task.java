3290279a1ee9dbe1d288daa31101da8fac8d4278
moschidisg <mosxidismosxidis@gmail.com>
2023-05-31 16:53:35 +0300

    create Task class

--- src/Task.java
+++ src/Task.java
@@ -1,13 +1,21 @@
 import java.util.ArrayList;
 
 public class Task {
+	static ArrayList<Task> tasksCatalog = new ArrayList<>();
+
 	private String name;
 	private double cost;
-	private ArrayList<Task> tasksCatalog = new ArrayList<>();
-	
-	
-	
+
 	public double getCost() {
 		return cost;
 	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Task> getSupportedTasksCatalog() {
+		return tasksCatalog;
+	}
+
 }
